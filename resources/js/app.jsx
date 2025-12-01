6cc10a2bde7372b42c73f5c30975ef625cdef7a5
Md. Mued Hassan <you@example.com>
2025-12-01 21:57:34 +0600

    working proper fix

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,7 +5,11 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    resolve: name => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: name =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx')
+        ),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />);
     },
